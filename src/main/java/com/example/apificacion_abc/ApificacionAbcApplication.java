66f59f56a926fd1da7642f96ea8ee647469c9540
ojcaje <ojcaje@gmail.com>
2023-09-18 16:21:13 -0400

    first commit

---
+++ src/main/java/com/example/apificacion_abc/ApificacionAbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.apificacion_abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApificacionAbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApificacionAbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apificacion_abc/ApificacionAbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apificacion_abc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApificacionAbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
